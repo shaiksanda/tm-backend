5498d15a9eb2558fec9c86daca6fac71b9109778
shaiksanda <shaiksandanibasha7@gmail.com>
2025-08-28 16:04:54 +0530

    Removed cors , allowed all origins

--- index.js
+++ index.js
@@ -20,19 +20,8 @@ const transporter = nodemailer.createTransport({
 const cors = require('cors');
 
 const app = express();
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true,
-    })
-);
+
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
